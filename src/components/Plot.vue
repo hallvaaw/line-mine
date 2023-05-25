848b3a627af528f44b48735891b5556b6a9fd563
hallvaaw <hallvard_wahler@hotmail.com>
2023-05-25 22:30:34 +0200

    added chartjs

---
+++ src/components/Plot.vue
@@ -0,0 +1,37 @@
+<template>
+  <Bar :data="data" :options="options" />
+</template>
+
+<script lang="ts">
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+import { Bar } from 'vue-chartjs'
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+
+export default {
+  name: 'App',
+  components: {
+    Bar
+  },
+  data() {
+    return {
+      data: {
+        labels: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'],
+        datasets: [{ data: [40, 20, 12, 49, 2, 1, 4, 29, 11, 50] }]
+      },
+      options: {
+        responsive: true,
+        indexAxis: 'y'
+      }
+    }
+  }
+}
+</script>
